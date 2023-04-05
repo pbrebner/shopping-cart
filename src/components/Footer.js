0a5f67ceeb2a0d3e865ddc59e45a0bad50c7ae1e
Patrick Brebner <patrick.brebner@gmail.com>
2023-04-05 14:59:43 -0700

    Initial commit

---
+++ src/components/Footer.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+function Footer() {
+    return (
+        <footer className="footer">
+            <p>Developed by Patrick Brebner</p>
+        </footer>
+    );
+}
+
+export default Footer;
---
+++ src/pages/Shop.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+function Shop() {
+    return (
+        <div className="shop">
+            <h2>Shop Page</h2>
+        </div>
+    );
+}
+
+export default Shop;
