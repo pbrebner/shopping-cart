14833ca13477317dc74d55538b079c08c6575e40
Patrick Brebner <patrick.brebner@gmail.com>
2023-04-05 19:25:52 -0700

    Create start shopping btn

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,10 +1,21 @@
 import React from "react";
+import { useNavigate } from "react-router-dom";
+
 import "../styles/Home.css";
 
 function Home() {
+    const navigate = useNavigate();
+
+    function navigateShop() {
+        navigate("/shop");
+    }
+
     return (
         <div className="home">
-            <h2>Home Page</h2>
+            <h2 className="pageHeading">Home Page</h2>
+            <button className="shopBtn" onClick={navigateShop}>
+                Start Shopping
+            </button>
         </div>
     );
 }
