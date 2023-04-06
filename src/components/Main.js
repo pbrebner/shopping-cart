aa3589ba9921eb2908b1c22b11767eae683be484
Patrick Brebner <patrick.brebner@gmail.com>
2023-04-05 19:26:59 -0700

    Fetch data for products

--- src/components/Main.js
+++ src/components/Main.js
@@ -3,13 +3,15 @@ import { Routes, Route } from "react-router-dom";
 import Home from "../pages/Home";
 import Shop from "../pages/Shop";
 import Contact from "../pages/Contact";
+import Product from "../pages/ProductDetail";
 
 function Main() {
     return (
         <main className="main">
             <Routes>
                 <Route path="/" exact Component={Home} />
-                <Route path="/shop" Component={Shop} />
+                <Route path="/shop" exact Component={Shop} />
+                <Route path="/shop/:id" Component={Product} />
                 <Route path="/contact" Component={Contact} />
             </Routes>
         </main>
--- src/pages/Shop.js
+++ src/pages/Shop.js
@@ -1,10 +1,39 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import { Link } from "react-router-dom";
 import "../styles/Shop.css";
 
 function Shop() {
+    const [productList, setProductList] = useState([]);
+    useEffect(() => {
+        loadProducts();
+    }, []);
+
+    async function loadProducts() {
+        setProductList(filterProducts(await fetchProducts()));
+    }
+
+    async function fetchProducts() {
+        const productData = await fetch("https://fakestoreapi.com/products");
+        const products = await productData.json();
+        return products;
+    }
+
+    const filterProducts = (products) => {
+        return products.filter(
+            (product) =>
+                product.category === `men's clothing` ||
+                product.category === `women's clothing`
+        );
+    };
+
     return (
         <div className="shop">
-            <h2>Shop Page</h2>
+            <h2 className="pageHeading">Shop Page</h2>
+            {productList.map((product) => (
+                <h3 key={product.id}>
+                    <Link to={`/shop/${product.id}`}>{product.title}</Link>
+                </h3>
+            ))}
         </div>
     );
 }
