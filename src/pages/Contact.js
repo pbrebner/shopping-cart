ef4879ed642f3635631395bce34a2c0d266d4761
Patrick Brebner <patrick.brebner@gmail.com>
2023-04-05 19:24:28 -0700

    Create contact layout

--- src/pages/Contact.js
+++ src/pages/Contact.js
@@ -1,10 +1,16 @@
 import React from "react";
 import "../styles/Contact.css";
+import ContactForm from "../components/ContactForm";
 
 function Contact() {
     return (
         <div className="contact">
-            <h2>Contact Page</h2>
+            <h2 className="pageHeading">Contact Us</h2>
+            <p>
+                Have a question? <strong>Email us</strong> using the form below
+                and someone will get back to you as soon as possible.
+            </p>
+            <ContactForm />
         </div>
     );
 }
