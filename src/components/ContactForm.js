0e5a0fb45c3098f5518bffeb26052376db5a5bda
Patrick Brebner <patrick.brebner@gmail.com>
2023-04-05 19:24:37 -0700

    Create contact form

---
+++ src/components/ContactForm.js
@@ -0,0 +1,37 @@
+import React from "react";
+import "../styles/Contact.css";
+
+function ContactForm() {
+    function handleSubmit(e) {
+        e.preventDefault();
+    }
+
+    //Need to add function to remove text values when submit is clicked
+
+    return (
+        <form className="contactForm" onSubmit={handleSubmit}>
+            <div className="formItem">
+                <input type={"text"} placeholder="First Name"></input>
+            </div>
+            <div className="formItem">
+                <input type={"text"} placeholder="Last Name"></input>
+            </div>
+            <div className="formItem colSpan2">
+                <input type={"email"} placeholder="Email"></input>
+            </div>
+            <div className="formItem colSpan2">
+                <input type={"text"} placeholder="Subject"></input>
+            </div>
+            <div className="formItem colSpan2">
+                <textarea rows="8" placeholder="Message"></textarea>
+            </div>
+            <div className="formItem colSpan2">
+                <button className="formSubmitBtn" type="submit">
+                    Submit
+                </button>
+            </div>
+        </form>
+    );
+}
+
+export default ContactForm;
