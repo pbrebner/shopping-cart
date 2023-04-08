7d22030062e141304fd7a47f478b9786a4f8a732
Patrick Brebner <patrick.brebner@gmail.com>
2023-04-07 21:15:55 -0700

    Create functions and state to modify cart

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import { BrowserRouter as Router } from "react-router-dom";
 import Header from "./components/Header";
 import Main from "./components/Main";
@@ -6,11 +6,53 @@ import Footer from "./components/Footer";
 import "./styles/App.css";
 
 function App() {
+    let [cartItems, setCartItems] = useState([]);
+    let [cartNumber, setCartNumber] = useState(0);
+
+    function incrementCart(incrementValue = 1) {
+        let currentNumber = cartNumber;
+        setCartNumber(currentNumber + incrementValue);
+    }
+
+    function decrementCart(decrementValue = 1) {
+        let currentNumber = cartNumber;
+        setCartNumber(currentNumber + decrementValue);
+    }
+
+    function addCartItem(productInfo, qty) {
+        console.log(productInfo);
+        let currentItemList = cartItems;
+        //Need to check current cart items and add to qty if item already in cart
+        for (const item of currentItemList) {
+            if (item.id === productInfo.id) {
+                item.qty = item.qty + qty;
+                //console.log(currentItemList);
+                setCartItems(currentItemList);
+                return;
+            }
+        }
+        //If no repeated item, append to list (id, title, price, numberOfItems)
+        currentItemList.push({
+            id: productInfo.id,
+            title: productInfo.title,
+            price: productInfo.price,
+            qty: qty,
+        });
+
+        //console.log(currentItemList);
+
+        setCartItems(currentItemList);
+    }
+
     return (
         <Router>
             <div className="app">
-                <Header />
-                <Main />
+                <Header cartNumber={cartNumber} />
+                <Main
+                    cartItems={cartItems}
+                    addCartItem={addCartItem}
+                    incrementCart={incrementCart}
+                />
                 <Footer />
             </div>
         </Router>
