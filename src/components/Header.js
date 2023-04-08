7ff3248549c39df474e3f17d56ed074b75824427
Patrick Brebner <patrick.brebner@gmail.com>
2023-04-07 21:14:16 -0700

    Add cart btn

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,12 +1,22 @@
 import React from "react";
+import { useNavigate } from "react-router-dom";
 import Nav from "./Nav";
 import "../styles/Header.css";
 
-function Header() {
+function Header({ cartNumber }) {
+    const navigate = useNavigate();
+
+    function navigateCart() {
+        navigate("/cart");
+    }
+
     return (
         <header className="header">
             <h1 className="storeName">Store Name</h1>
             <Nav />
+            <button className="cartBtn" onClick={navigateCart}>
+                {cartNumber} Cart
+            </button>
         </header>
     );
 }
