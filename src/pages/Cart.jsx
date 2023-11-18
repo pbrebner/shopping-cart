7bbc6207c58a217bbe50c54d3370f97bbf567dce
Patrick Brebner <patrick.brebner@gmail.com>
2023-11-17 19:51:07 -0800

    Created cart page

---
+++ src/pages/Cart.jsx
@@ -0,0 +1,11 @@
+import { useState, useEffect } from "react";
+
+function Cart() {
+    return (
+        <div className="cartPage">
+            <h2>This is the Cart Page</h2>
+        </div>
+    );
+}
+
+export default Cart;
