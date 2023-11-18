9d12e7c9d1898ff8aea1056f1eb28a0510240d6f
Patrick Brebner <patrick.brebner@gmail.com>
2023-11-17 20:04:42 -0800

    Build app layout

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,24 @@
 import { useState } from "react";
+import { BrowserRouter as Router } from "react-router-dom";
+import Header from "./components/Header";
+import Main from "./components/Main";
+import Footer from "./components/Footer";
 
 import "./App.css";
 
 function App() {
-    return <></>;
+    let [cartItems, setCartItems] = useState([]);
+    let [cartNumber, setCartNumber] = useState(0);
+
+    return (
+        <Router>
+            <div className="app">
+                <Header cartNumber={cartNumber} />
+                <Main />
+                <Footer />
+            </div>
+        </Router>
+    );
 }
 
-export { App };
+export default App;
--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,11 +1,21 @@
 import { Routes, Route } from "react-router-dom";
 import Home from "../pages/Home";
+import Shop from "../pages/Shop";
+import Contact from "../pages/Contact";
+import ProductDetails from "../pages/ProductDetails";
+import Cart from "../pages/Cart";
+
+import "./styles/main.css";
 
 function Main() {
     return (
         <main className="main">
             <Routes>
                 <Route path="/" exact element={<Home />} />
+                <Route path="/shop" exact element={<Shop />} />
+                <Route path="/shop/:id" element={<ProductDetails />} />
+                <Route path="/cart" element={<Cart />} />
+                <Route path="/contact" element={<Contact />} />
             </Routes>
         </main>
     );
