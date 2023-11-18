70590cc1a4cd8d223cccea860b946ca7f60407c0
Patrick Brebner <patrick.brebner@gmail.com>
2023-11-17 16:52:50 -0800

    Create header

---
+++ src/components/Header.jsx
@@ -0,0 +1,27 @@
+import { useNavigate } from "react-router-dom";
+
+import Nav from "./Nav";
+import cartIcon from "../assets/shopping-cart.png";
+
+function Header({ cartNumber }) {
+    const navigate = useNavigate();
+
+    function navigateCart() {
+        navigate("/cart");
+    }
+
+    return (
+        <header className="header">
+            <div className="headerInnerWrapper">
+                <h1 className="storeName">Store Name</h1>
+                <Nav />
+                <button className="cartBtn" onClick={navigateCart}>
+                    {cartNumber}
+                    <img className="cartIcon" src={cartIcon} alt="Cart Icon" />
+                </button>
+            </div>
+        </header>
+    );
+}
+
+export default Header;
