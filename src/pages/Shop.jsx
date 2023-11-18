3f4a85676b8caee9cfa8d09d203a69b9bee7df46
Patrick Brebner <patrick.brebner@gmail.com>
2023-11-17 19:51:54 -0800

    Created shop page

---
+++ src/pages/Shop.jsx
@@ -0,0 +1,11 @@
+import { useState, useEffect } from "react";
+
+function Shop() {
+    return (
+        <div className="shopPage">
+            <h2>This is the Shop Page</h2>
+        </div>
+    );
+}
+
+export default Shop;
