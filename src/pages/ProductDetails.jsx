422cc655ef3a1ff965e059b59f6bea902c7c8896
Patrick Brebner <patrick.brebner@gmail.com>
2023-11-17 19:52:17 -0800

    Created product details page

---
+++ src/pages/ProductDetails.jsx
@@ -0,0 +1,11 @@
+import { useState, useEffect } from "react";
+
+function ProductDetails() {
+    return (
+        <div className="productDetailsPage">
+            <h2>This is the product details page</h2>
+        </div>
+    );
+}
+
+export default ProductDetails;
